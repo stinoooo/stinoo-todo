4b7576f2253fba5b50b40971b534b90d67eb582f
stinoooo <stabbajabba@gmail.com>
2025-02-13 18:11:17 +0100

    code rewrite
    Signed-off-by: stinoooo <stabbajabba@gmail.com>
    Signed-off-by: stinoooo <6011558@mborijnland.nl>

---
+++ components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div
+        class="flex flex-row items-start gap-4 rounded-lg p-4 bg-gradient-to-b from-slate-900 to-slate-950 border border-slate-800"
+    >
+        <p class="w-0 flex-1"><slot /></p>
+        <button
+            class="ml-auto text-sm font-medium px-3 py-1 rounded-md bg-red-600 shadow-lg shadow-red-400/50"
+            @click="() => $emit('remove')"
+        >
+            Delete
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineEmits<{
+    (e: "remove"): void;
+}>();
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1 class="font-bold text-2xl">About</h1>
+        <p class="text-slate-400 mt-2">
+            This is a simple todo app built with Nuxt.js 3 and Tailwind CSS. The app has been made by Stino.
+        </p>
+        <div class="flex flex-row gap-4 mt-4">
+            <a
+                href="https://github.com/stinoooo/stinoo-todo"
+                rel="noreferrer noopener"
+                class="px-6 py-1.5 font-medium text-sm text-slate-900 bg-cyan-300 rounded-lg transition-colors hover:bg-cyan-400"
+            >
+                Github
+            </a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+useSeoMeta({
+    title: "About",
+});
+
+definePageMeta({
+    layout: "dashboard",
+});
+</script>
